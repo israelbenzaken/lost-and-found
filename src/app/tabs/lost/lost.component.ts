a8ac77416a41f0321c11850fd5b920cd0781e819
israelbenzaken <israelbz10@gmail.com>
2017-05-24 19:57:49 +0300

    lostAfound245

--- src/app/tabs/lost/lost.component.ts
+++ src/app/tabs/lost/lost.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit,ViewChild,ElementRef, NgZone } from '@angular/core';
+import { NgForm ,FormControl} from '@angular/forms';
+import { MapsAPILoader} from 'angular2-google-maps/core';
+import {} from '@types/googlemaps';
+
+declare var google: any;
 
 @Component({
-  selector: 'app-lost',
-  templateUrl: './lost.component.html',
-  styleUrls: ['./lost.component.css']
+	selector: 'app-lost',
+	templateUrl: './lost.component.html',
+	styleUrls: ['./lost.component.css']
 })
 export class LostComponent implements OnInit {
+
+	// maps
+	public latitude: number;
+	public longitude: number;
+	public searchControl: FormControl;
+	public zoom: number;
+	@ViewChild("search")
+	public searchElementRef: ElementRef;
+
 
 	@ViewChild('f') signupForm: NgForm;
 	Anotherchoice = '';
@@ -18,12 +31,57 @@ export class LostComponent implements OnInit {
 	};
 	submitted = false;
 
-  constructor() { }
+	constructor(
+		private mapsAPILoader: MapsAPILoader,
+		private ngZone: NgZone
+		) {}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.zoom = 4;
+		this.latitude = 39.8282;
+		this.longitude = -98.5795;
 
-  onSubmit() {
+		//create search FormControl
+		this.searchControl = new FormControl();
+
+		//set current position
+		this.setCurrentPosition();
+
+		//load Places Autocomplete
+		this.mapsAPILoader.load().then(() => {
+			let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+				types: ["address"]
+			});
+			autocomplete.addListener("place_changed", () => {
+				this.ngZone.run(() => {
+					//get the place result
+					let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+					//verify result
+					if (place.geometry === undefined || place.geometry === null) {
+						return;
+					}
+
+					//set latitude, longitude and zoom
+					this.latitude = place.geometry.location.lat();
+					this.longitude = place.geometry.location.lng();
+					this.zoom = 12;
+				});
+			});
+		});
+	}
+
+	private setCurrentPosition() {
+		if ("geolocation" in navigator) {
+			navigator.geolocation.getCurrentPosition((position) => {
+				this.latitude = position.coords.latitude;
+				this.longitude = position.coords.longitude;
+				this.zoom = 14;
+			});
+		}
+	}
+
+	onSubmit() {
 		this.submitted = true;
 		this.user.objType = this.signupForm.value.objType;
 		this.user.Description = this.signupForm.value.Description;
